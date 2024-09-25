b43af8e42484e91bfcace4dd7eb98d4415bc6ef7
itsmariebtw <61733813+amdemano@users.noreply.github.com>
2024-09-26 00:28:26 +0800

    Create lower.dart
    added lower exercises with clickable view?

---
+++ lib/exercise/lower.dart
@@ -0,0 +1,36 @@
+// lib/lower_body_page.dart
+import 'package:flutter/material.dart';
+import 'exercard.dart'; // Import the ExerciseCard
+
+class LowerBodyPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Lower Body Exercises'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ExerciseCard(
+              title: 'Bench Press',
+              onTap: () {
+                // Navigate to Bench Press details page
+              },
+            ),
+            SizedBox(height: 20),
+            ExerciseCard(
+              title: 'Shoulder Press',
+              onTap: () {
+                // Navigate to Shoulder Press details page
+              },
+            ),
+            SizedBox(height: 20),
+          ],
+        ),
+      ),
+    );
+  }
+}
