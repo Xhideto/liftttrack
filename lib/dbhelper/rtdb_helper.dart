9fc6cfce349dd9588a95b92639da7cfcd2bd618c
xerayx <202111173@fit.edu.ph>
2024-09-29 19:45:11 +0800

    DB helper for flutter
    This resolves #2
    
    Remind you to use ``pub get`` to update the flutter dependencies on this branch update.

---
+++ lib/dbhelper/rtdb_helper.dart
@@ -0,0 +1,30 @@
+import 'dart:convert';
+import 'package:liftttrack/dataTemplate/user_data.dart';
+import 'package:firebase_database/firebase_database.dart';
+
+class RTDBHelper {
+  final DatabaseReference _userRef = FirebaseDatabase.instance.ref('users/?id').child('users');
+
+  // Method to add data
+  Future<void> addUser(User user) async {
+    final newUser = _userRef.push();
+    await newUser.set(jsonEncode(user));
+  }
+
+  // Method to update data
+  Future<void> updateUser(String key, dynamic value) async {
+    await _userRef.child(key).update(value);
+  }
+
+  // Method to delete data
+  Future<void> deleteUser(String key) async {
+    await _userRef.child(key).remove();
+  }
+
+  // Method to get data
+  Future<User> getUser(String id) async {
+    final snapshot = await _userRef.child(id).once();
+    final user = jsonDecode(snapshot as String) as Map<String, dynamic>;
+    return User.fromJson(user);
+  }
+}
