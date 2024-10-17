70ea2546c52733fac96e9b44f715c5395063b0c6
Dojima Shinji <100776572+Xhideto@users.noreply.github.com>
2024-10-17 20:50:14 +0800

    Update exer.dart

--- lib/navigation/exer.dart
+++ lib/navigation/exer.dart
@@ -1,66 +1,100 @@
-// lib/exercise_page.dart
 import 'package:flutter/material.dart';
-import '../exercise/exercard.dart'; // Import the ExerciseCard
-import '../exercise/upper.dart';
-import '../exercise/lower.dart';
+// import 'package:lifttrack/exercise/exercard.dart';
+import 'package:lifttrack/exercise/lower.dart';
+import 'package:lifttrack/exercise/upper.dart';
 
-import 'bar.dart'; // Import the UpperBodyPage
-
-class ExercisePage extends StatefulWidget {
-  @override
-  _ExercisePageState createState() => _ExercisePageState();
-}
-
-class _ExercisePageState extends State<ExercisePage> {
-  int _selectedIndex = 1; // Set the initial index to 1 for ExercisePage
-
-  void _onItemTapped(int index) {
-    if (index == 0 || index == 2) {
-      Navigator.pop(context); // Go back to HomeScreen
-    } else {
-      setState(() {
-        _selectedIndex = index;
-      });
-    }
-  }
+class ExercisePage extends StatelessWidget {
+  const ExercisePage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('EXERCISES'),
+        title: Text('EXERCISE', style: TextStyle(color: Colors.white)), // White text
+        backgroundColor: Colors.deepOrange.shade400,
+        elevation: 3,
         automaticallyImplyLeading: false, // Remove the back button
       ),
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
+        padding: const EdgeInsets.all(24.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            ExerciseCard(
-              title: 'UPPER',
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => UpperBodyPage()),
-                );
-              },
+            Center(
+              child: Text(
+                'SELECT A MUSCLE GROUP',
+                style: TextStyle(
+                  fontSize: 29,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.deepOrange[600],
+                ),
+              ),
             ),
-            SizedBox(height: 20),
-            ExerciseCard(
-              title: 'LOWER',
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => LowerBodyPage()),
-                );
-              },
+            const SizedBox(height: 70),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  width: 280, // Fixed width for the cards
+                  child: GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => UpperBodyPage()),
+                      );
+                    },
+                    child: Container(
+                      padding: const EdgeInsets.all(24.0),
+                      decoration: BoxDecoration(
+                        color: Colors.amber[800], // Button color
+                        borderRadius: BorderRadius.circular(16),
+                      ),
+                      child: Center(
+                        child: Text(
+                          'UPPER BODY', // Updated text
+                          style: TextStyle(
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white, // Text color
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 30),
+                SizedBox(
+                  width: 280, // Fixed width for the cards
+                  child: GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => LowerBodyPage()),
+                      );
+                    },
+                    child: Container(
+                      padding: const EdgeInsets.all(24.0),
+                      decoration: BoxDecoration(
+                        color: Colors.amber[800], // Button color
+                        borderRadius: BorderRadius.circular(16),
+                      ),
+                      child: Center(
+                        child: Text(
+                          'LOWER BODY', // Updated text
+                          style: TextStyle(
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white, // Text color
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
           ],
         ),
-      ),
-      bottomNavigationBar: CustomBottomNavigationBar(
-        selectedIndex: _selectedIndex,
-        onItemTapped: _onItemTapped,
       ),
     );
   }
